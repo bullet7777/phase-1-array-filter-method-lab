82597d6a869eb2a6f4d69eae17820d5aa2964e39
chris <chris.mahfouz@hotmail.com>
2021-03-19 12:55:53 +0200

    completed assignment filter

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here
+
+
+function findMatching(arrayOfDrivers,name){
+
+function filterDrivers(currentValue){
+    var test=currentValue.toLowerCase() === name.toLowerCase()
+return test
+}
+
+    var matchingDrivers =arrayOfDrivers.filter(filterDrivers)
+
+    return matchingDrivers
+}
+
+function fuzzyMatch(arrayOfDrivers,letters){
+
+    function beginLetter(currentValue){
+var startingLetter=currentValue[0]===letters[0]
+        return startingLetter
+    }
+var allDriversWithProvidedLetter=arrayOfDrivers.filter(beginLetter)
+
+return allDriversWithProvidedLetter
+}
+
+
+function matchName(arrayOfDrivers,driverName){
+
+function namesOfDrivers(currentValue){
+var test=currentValue.name===driverName
+
+return test
+}
+
+    var arrayOfObjects=arrayOfDrivers.filter(namesOfDrivers)
+
+    return arrayOfObjects
+
+}
+
+
+
+
+
+
+
+
+
+
+
